84a4e39dfd8ef34e6b54a7dd0d48fddefd273484
Andres Jimenez <andres.jimenez@miliopay.com>
2023-07-20 11:52:31 -0500

    [MODIFY: message]

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello Movies Amazing!"', () => {
-      expect(appController.getHello()).toBe('Hello Movies Amazing');
+      expect(appController.getHello()).toBe('Hello Movies Amazing!!');
     });
   });
 });
